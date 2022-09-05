6b49e49c3cbdeeacc617f4e1dd8a1a56c89959bd
vitalii.khmelko <vitalii.khmelko@fungible.com>
2022-09-05 15:30:12 -0400

    Bubble algorithm improved

--- js-algorithms/bubble-optimized.js
+++ js-algorithms/bubble-optimized.js
@@ -1,17 +1,22 @@
-// Most inefficient sorting algorithm. It's called bubble because elements
-// with small weight (1, 2, 3) bubble up, While heavier go down (8, 9)
-// time complexity O(n^2)
-// Auxiliary Space: O(1)
+// Improve bubble algorithm:
+// 1. Avoid extra loop if only one unsorted element left (it was sorted before with other components)
+// 2. Each next loop has a smaller amount of iterations because elements at the end of array are in their places
+// 3. Stop looping if elements are already sorted.
 
 const bubbleOptimizedSorting = () => {
   const arr = [5, 3, 7, 6, 8, 1, 2];
+  // const arr = [1, 2, 3, 5, 6, 7, 8];
   let iterations = 0;
 
   console.log("initial array " + arr.join(", "));
 
-  for (let loop = 0; loop < arr.length; loop++) {
+  // Don't run a loop if only one component left. It is obvious that it was compared before
+  for (let loop = 0; loop < arr.length - 1; loop++) {
     console.log(`Loop #${loop + 1}`);
-    for (let index = 0; index < arr.length; index++) {
+
+    let swapsInLoop = 0;
+    // Ignore elements that already been sorted and now are in their places at the end of array
+    for (let index = 0; index < arr.length - loop - 1; index++) {
       iterations++;
 
       const elementAbove = arr[index];
@@ -21,9 +26,14 @@ const bubbleOptimizedSorting = () => {
         console.log(`swapping ${elementAbove} and ${elementBottom}`);
         arr[index] = elementBottom;
         arr[index + 1] = elementAbove;
+        swapsInLoop++;
       } else {
         console.log(`don't swap ${elementAbove} and ${elementBottom}`);
       }
+    }
+    if (swapsInLoop === 0) {
+      console.log("elements are sorted. Don't need to run other loops");
+      break;
     }
     console.log(arr.join(", "));
   }
